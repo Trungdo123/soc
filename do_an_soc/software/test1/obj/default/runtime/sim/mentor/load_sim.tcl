24abae52e17f341a87849061d5b4a49d20d952a7
Trungdo123 <ngoctrung1c1@gmail.com>
2022-06-03 18:32:05 +0700

    debug

--- do_an_soc/software/test1/obj/default/runtime/sim/mentor/load_sim.tcl
+++
@@ -1,8 +0,0 @@
-# ------------------------------------------------------------------------------
-# Top Level Simulation Script to source msim_setup.tcl
-# ------------------------------------------------------------------------------
-set QSYS_SIMDIR obj/default/runtime/sim
-source msim_setup.tcl
-# Copy generated memory initialization hex and dat file(s) to current directory
-file copy -force C:/Users/ngoct/Desktop/do_an_soc/software/test1/mem_init/hdl_sim/system_onchip_memory2_0.dat ./ 
-file copy -force C:/Users/ngoct/Desktop/do_an_soc/software/test1/mem_init/system_onchip_memory2_0.hex ./ 
